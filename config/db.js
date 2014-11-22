701755817a6291dcfddb7badac1dd748363fe3e0
Hayk Pilosyan <hpilosyan@macadamian.com>
2014-11-22 13:45:57 +0400

    Fix production db credentials.

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 var mongo = require('mongoskin');
 var config = require('./config');
 
-var credentials = config.db.username && config.db.password ? config.db.username + config.db.password + '@' : '';
+var credentials = config.db.username && config.db.password ? config.db.username + ':' +  config.db.password + '@' : '';
 var db = mongo.db('mongodb://' + credentials + config.db.host + ':' + config.db.port + config.db.database, {native_parser: false});
 
 module.exports = db;
