ca24fc4cb20bfd7f13372e7b62296554d954b0f6
Hayk Pilosyan <hpilosyan@macadamian.com>
2014-11-23 01:57:42 +0400

    Add global collection of pills.

--- models/pill.js
+++ models/pill.js
@@ -8,11 +8,39 @@ module.exports = {
   add_pill: function (device_token, name) {
     var deferred = promise.defer();
 
-    db.collection('device').update({token: device_token}, {'$push': {pills: name}}, function (err, device) {
+    db.collection('device').update({token: device_token}, {'$addToSet': {pills: name}}, function (err, device) {
       if (err) {
         deferred.reject(err);
       } else {
         deferred.resolve(device);
+      }
+    });
+
+    return deferred.promise;
+  },
+
+  all_pills: function () {
+    var deferred = promise.defer();
+
+    db.collection('pill').find().toArray(function (err, pills) {
+      if (err) {
+        deferred.reject(err);
+      } else {
+        deferred.resolve(pills);
+      }
+    });
+
+    return deferred.promise;
+  },
+
+  add_to_pill_collection: function (name) {
+    var deferred = promise.defer();
+
+    db.collection('pill').save({name: name}, function (err, result) {
+      if (err) {
+        deferred.reject(err);
+      } else {
+        deferred.resolve(result);
       }
     });
 
--- routes/pills.js
+++ routes/pills.js
@@ -9,6 +9,16 @@ router.post('/:device_token/:pill_name', function(req, res, next) {
   var pill_name = req.params.pill_name;
 
   pill.add_pill(device_token, pill_name).then(function(result) {
+    // Add pill to global pill collection
+    pill.add_to_pill_collection(pill_name);
+    res.json(result);
+  }, function (err) {
+    next(err);
+  });
+});
+
+router.get('/', function(req, res, next) {
+  pill.all_pills().then(function(result) {
     res.json(result);
   }, function (err) {
     next(err);
