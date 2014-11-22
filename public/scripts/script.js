ba9ba1c9146492a77994522a38e86c739618a2a3
machackyeghishe <yeghishe.amirkhanyan@gmail.com>
2014-11-22 22:22:39 +0400

    Edit tab js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -21,6 +21,10 @@ function attachEvents()
 {
 	//Common
 	$("#deviceSelect").change(onDevicesSelectChange);
+	$("li.pills").click(onPillsTabClick);
+	$("li.schedule").click(onScheduleTabClick);
+	$("li.shopping-card").click(onShoppingCardTabClick);
+	$("li.sos").click(onSosTabClick);
 	
 	//Schedule
 	$("#addTimestamp1").click(function(){onAddTimestampClick(BOX_1)});
@@ -33,7 +37,9 @@ function attachEvents()
 	$("#saveScheduleButton3").click(function(){onSaveScheduleButtonClick(BOX_3)});
 		
 	//Pills
-	$("#savePillsButton1").click();
+	$("#savePillsButton1").click(function(){onSavePillsButtonClick(BOX_1)});
+	$("#savePillsButton2").click(function(){onSavePillsButtonClick(BOX_2)});
+	$("#savePillsButton3").click(function(){onSavePillsButtonClick(BOX_3)});
 }
 
 function getDevicesList()
@@ -100,7 +106,9 @@ function onSaveScheduleButtonClick(selectedBox)
 			//Get interval
 			var interval1=$("#interval1").val();
 			
-			//Get timestamps
+			//TODO: Get timestamps
+			
+			//TODO: ajax request to save
 			break;
 		}
 		case BOX_2:
@@ -111,7 +119,9 @@ function onSaveScheduleButtonClick(selectedBox)
 			//Get interval
 			var interval2=$("#interval2").val();
 			
-			//Get timestamps
+			//TODO: Get timestamps
+			
+			//TODO: ajax request to save
 			break;
 		}
 		case BOX_3:
@@ -122,8 +132,101 @@ function onSaveScheduleButtonClick(selectedBox)
 			//Get interval
 			var interval3=$("#interval3").val();
 			
-			//Get timestamps
+			//TODO: Get timestamps
+			
+			//TODO: ajax request to save
 			break;
 		}
 	}
+}
+
+function onSavePillsButtonClick(selectedBox)
+{
+	switch(selectedBox)
+	{
+		case BOX_1:
+		{
+			var box1Id=parseInt($("#box1_id").val());
+			//Get medicine name
+			var medicineName=$("#medicineNameInput1").val();
+			//Get pills quantity
+			var pillsQuantity=$("#pillsQuantityInput1").val();
+			
+			//TODO: ajax request to save
+			break;
+		}
+		case BOX_2:
+		{
+			var box2Id=parseInt($("#box2_id").val());
+			//Get medicine name
+			var medicineName=$("#medicineNameInput2").val();
+			//Get pills quantity
+			var pillsQuantity=$("#pillsQuantityInput2").val();
+			
+			//TODO: ajax request to save
+			break;
+		}
+		case BOX_3:
+		{
+			var box3Id=parseInt($("#box3_id").val());
+			//Get medicine name
+			var medicineName=$("#medicineNameInput3").val();
+			//Get pills quantity
+			var pillsQuantity=$("#pillsQuantityInput3").val();
+			
+			//TODO: ajax request to save
+			break;
+		}
+	}
+}
+
+function onPillsTabClick()
+{
+	//Set the header
+	$("h1.cur-title").html("Pills");
+	//Highlight
+	$("div.med-menu span").removeClass("cur");
+	$("li.pills > span").addClass("cur");
+	
+	$("#schedule-boxes, #shopping-card-boxes, #sos-boxes, #pil-boxes").css("display", "none");
+	$("#pil-boxes").css("display", "block");
+}
+
+function onScheduleTabClick()
+{
+	//Set the header
+	$("h1.cur-title").html("Schedule");
+	
+	//Highlight
+	$("div.med-menu span").removeClass("cur");
+	$("li.schedule > span").addClass("cur");
+	
+	$("#schedule-boxes, #shopping-card-boxes, #sos-boxes, #pil-boxes").css("display", "none");
+	$("#schedule-boxes").css("display", "block");
+}
+
+function onShoppingCardTabClick()
+{
+	//Set the header
+	$("h1.cur-title").html("Shopping Card");
+	
+	//Highlight
+	$("div.med-menu span").removeClass("cur");
+	$("li.shopping-card > span").addClass("cur");
+	
+	$("#schedule-boxes, #shopping-card-boxes, #sos-boxes, #pil-boxes").css("display", "none");
+	$("#shopping-card-boxes").css("display", "block");
+}
+
+function onSosTabClick()
+{
+	//Set the header
+	$("h1.cur-title").html("SOS");
+	
+	//Highlight
+	$("div.med-menu span").removeClass("cur");
+	$("li.sos > span").addClass("cur");
+	
+	$("#schedule-boxes, #shopping-card-boxes, #sos-boxes, #pil-boxes").css("display", "none");
+	$("#sos-boxes").css("display", "block");
 }
